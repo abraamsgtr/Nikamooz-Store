d9908cda3c06ac983d0fa94b8e5e53e9a94385b8
mohammad141 <am823617@gmail.com>
2019-09-24 22:29:59 +0330

    Added Entities

---
+++ NikamoozStore.Core.Domain/Categories/Category.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace NikamoozStore.Core.Domain.Categories
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
---
+++ NikamoozStore.Core.Domain/Products/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace NikamoozStore.Core.Domain.Categories
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
